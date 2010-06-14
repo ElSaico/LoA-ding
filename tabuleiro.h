42bcb2bcf2b9ae156185040bc97f2224d6244e0a
El Saico <bomarques@inf.ufrgs.br>
2010-06-13 23:56:59 -0300

    Adicionada magia negra para descobrir posição de um bit

--- tabuleiro.h
+++ tabuleiro.h
@@ -11,13 +11,15 @@
 #define LIMITE_O    0x7F7F7F7F7F7F7F7FULL
 #define LIMITE_L    0xFEFEFEFEFEFEFEFEULL
 
-#define pos(x,y) (linha(x) & coluna(y))
+#define pos(x,y) (0x8000000000000000ULL >> (((x) << 3)+(y)))
 #define coord(t,x,y) (t & pos(x, y))
 #define count(x) __builtin_popcountll(x)
 #define grid(t) (t.pecas[J_BRANCO] ^ t.pecas[J_PRETO])
 #define linha(x) (0xFF00000000000000ULL >> ((x) << 3))
 #define coluna(x) (0x8080808080808080ULL >> (x))
 #define adv(t) (t == J_BRANCO ? J_PRETO : J_BRANCO)
+#define linhaPos(t) ((t) & 0x0111)
+#define colunaPos(t) ((t) >> 3)
 
 typedef enum {
 	J_BRANCO = 0,
@@ -36,5 +38,6 @@ void move(Tabuleiro* t, uint64_t or, uint64_t d);
 uint64_t movePara(Tabuleiro t, uint64_t or);
 int nGrupos(uint64_t t);
 bool vitoria(uint64_t t);
+uint8_t indiceBit(uint64_t t);
 
 #endif
